7a91c40289f71824fd28958ad3bcffbdc32ef527
SayedY00 <elsayedmoharam000@gmai.com>
2023-05-18 16:34:15 +0400

    added some animations

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+window.onload = function () {
+    document.querySelector("img").style.cssText = "right:-50px; transform:rotate(-22deg) translateY(-50%)";
+    document.querySelector("button").style.cssText = "left:30px"
+    document.querySelector("h2").style.cssText = "left:30px"
+    console.log("tes")
+}
